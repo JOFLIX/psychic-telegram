85ef8d95e932dc23e3b28893a85019382af248d8
JOFLIX <joflixooko@outlook.com>
2019-08-25 05:40:55 +0300

    added deployment

--- django_photo_gallery/settings.py
+++ django_photo_gallery/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8ace3072-47a0-4910-b522-dc3601f38c35'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost',]
 INTERNAL_IPS = ('0.0.0.0','127.0.0.1','localhost',)
